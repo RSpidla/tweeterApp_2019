a3b0f91655fac58c8fda2d4dad016e4bfe75e010
‘RSpidla’ <rspidla@gmail.com>
2019-02-21 05:26:01 +0000

    Removed commented out code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// Basic express setup:
-
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
@@ -10,32 +8,8 @@ const app           = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
-// const db = require("./lib/in-memory-db");
-
 const MongoClient = require('mongodb').MongoClient;
 const MONGODB_URI = 'mongodb://localhost:27017/tweeter';
-
-
-
-
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-
-
-// Mount the tweets routes at the "/tweets" path prefix:
-// app.use("/tweets", tweetsRoutes);
-
-
 
 MongoClient.connect(
   MONGODB_URI,
